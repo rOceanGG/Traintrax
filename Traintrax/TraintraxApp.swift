873d70584f6593b30b64aff14eb008c801fe1a1c
Roshan G. Ganithi <zcabrgg@ucl.ac.uk>
2025-06-10 00:29:55 +0100

    Initial Commit

---
+++ Traintrax/TraintraxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraintraxApp.swift
+//  Traintrax
+//
+//  Created by Roshan Gani Ganithi on 10/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TraintraxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TraintraxTests/TraintraxTests.swift
@@ -0,0 +1,16 @@
+//
+//  TraintraxTests.swift
+//  TraintraxTests
+//
+//  Created by Roshan Gani Ganithi on 10/06/2025.
+//
+
+import Testing
+
+struct TraintraxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
